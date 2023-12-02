94d8edfeb71bbe41ea5b1fe0d00e14bc8def876a
Antonino Fugazzotto <afugcse@gmail.com>
2023-12-02 11:18:26 -0800

    2023 Day 2 part 1

--- 2023/02/main.go
+++ 2023/02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -23,22 +24,108 @@ func main() {
 	fmt.Printf("Input parsed in %s\n", post.Sub(pre))
 
 	pre, ans, post := time.Now(), part1(parsedData), time.Now()
-	expectedAns := 54940
+	expectedAns := 2512
 	fmt.Printf("Part1 answer: %d, in %s, pass: %t\n", ans, post.Sub(pre), ans == expectedAns)
 
 	pre, ans, post = time.Now(), part2(parsedData), time.Now()
-	expectedAns = 54208
+	expectedAns = 0
 	fmt.Printf("Part2 answer: %d, in %s, pass: %t\n", ans, post.Sub(pre), ans == expectedAns)
 }
 
-func parseInput(input string) any {
-	return nil
+type set = map[string]int
+
+type game struct {
+	id   int
+	sets []set
 }
 
-func part1(parsedData any) any {
-	return nil
+type games = []game
+
+var colorsMaxVal = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
 }
 
-func part2(parsedData any) any {
-	return nil
+func parseInput(input string) games {
+	lines := strings.Split(input, "\n")
+
+	allGames := games{}
+	for _, line := range lines {
+		fields := strings.Fields(line)[1:]
+
+		gameId := 0
+		sets := []set{}
+		s := set{}
+		i := 0
+		for i < len(fields) {
+			// It's the game ID
+			if i == 0 {
+				gId, err := strconv.Atoi(strings.TrimSuffix(fields[i], ":"))
+				if err != nil {
+					panic(fmt.Errorf("cannot parse input game id: %s", err.Error()))
+				}
+				gameId = gId
+				i++
+				continue
+			}
+
+			// fields[i] is the number and fields[i+1] is the color
+			val, err := strconv.Atoi(fields[i])
+			if err != nil {
+				panic(fmt.Errorf("cannot parse input set value: %s", err.Error()))
+			}
+			color := strings.TrimSuffix(strings.TrimSuffix(fields[i+1], ","), ";")
+			s[color] = val
+
+			isLastColorInSet := strings.Contains(fields[i+1], ";")
+			if isLastColorInSet {
+				sets = append(sets, s)
+				s = set{}
+			}
+
+			i += 2
+		}
+
+		if len(s) > 0 {
+			sets = append(sets, s)
+		}
+
+		g := game{
+			id:   gameId,
+			sets: sets,
+		}
+
+		allGames = append(allGames, g)
+	}
+
+	return allGames
+}
+
+func part1(parsedData games) int {
+	gameIDsSum := 0
+	for _, g := range parsedData {
+		isGameValid := true
+		for _, s := range g.sets {
+			for k, v := range s {
+				if v > colorsMaxVal[k] {
+					isGameValid = false
+					break
+				}
+			}
+			if !isGameValid {
+				break
+			}
+		}
+
+		if isGameValid {
+			gameIDsSum += g.id
+		}
+	}
+
+	return gameIDsSum
+}
+
+func part2(parsedData games) int {
+	return 0
 }
